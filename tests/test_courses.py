ab7436617bec839993821fd75692c1154842e6d4
knigoed <koldunstvo2001b@gmail.com>
2026-02-01 16:52:29 +0200

    Фикстуры Pytest

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,51 +1,26 @@
 import pytest
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import expect, Page
 
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
+def test_empty_courses_list(chromium_page_with_state: Page):
+    page = chromium_page_with_state
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    course_header = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
+    expect(course_header).to_be_visible()
+    expect(course_header).to_have_text('Courses')
 
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user.name@gmail.com')
+    there_is_no_results_header = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
+    expect(there_is_no_results_header).to_be_visible()
+    expect(there_is_no_results_header).to_have_text('There is no results')
 
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill('username')
+    empty_block_icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
+    expect(empty_block_icon).to_be_visible()
 
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill('password')
-
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-
-        context.storage_state(path="browser-state.json")
-
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state="browser-state.json")  # Указываем файл с сохраненным состоянием
-        page = context.new_page()
-
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-
-        course_header = page.get_by_test_id('courses-list-toolbar-title-text')
-        expect(course_header).to_be_visible()
-        expect(course_header).to_have_text('Courses')
-
-        there_is_no_results_header = page.get_by_test_id('courses-list-empty-view-title-text')
-        expect(there_is_no_results_header).to_be_visible()
-        expect(there_is_no_results_header).to_have_text('There is no results')
-
-        empty_block_icon = page.get_by_test_id('courses-list-empty-view-icon')
-        expect(empty_block_icon).to_be_visible()
-
-        results_will_be_displayed_here_header = page.get_by_test_id('courses-list-empty-view-description-text')
-        expect(results_will_be_displayed_here_header).to_be_visible()
-        expect(results_will_be_displayed_here_header).to_have_text(
-            'Results from the load test pipeline will be displayed here')
+    results_will_be_displayed_here_header = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
+    expect(results_will_be_displayed_here_header).to_be_visible()
+    expect(results_will_be_displayed_here_header).to_have_text(
+        'Results from the load test pipeline will be displayed here')
 
