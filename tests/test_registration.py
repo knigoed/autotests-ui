8ca0861bbdb14cb94f674896347e4fc95b6e33d0
knigoed <koldunstvo2001b@gmail.com>
2026-02-06 22:06:49 +0200

    Знакомство с PageObject

---
+++ tests/test_registration.py
@@ -0,0 +1,15 @@
+import pytest
+from pages.dashboard_page import DashboardPage
+from pages.registration_page import RegistrationPage
+
+
+@pytest.mark.regression
+@pytest.mark.registration
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage) -> None:
+    registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    registration_page.fill_registration_form()
+    registration_page.click_registration_button()
+    dashboard_page.check_dashboard_title()
+
+
+
