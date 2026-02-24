565acc29af8bb9168b7d9bdba062096b3e8688fc
knigoed <koldunstvo2001b@gmail.com>
2026-02-24 19:45:38 +0200

    Наращивание тестовой базы и рефакторинг

--- pages/authentication/registration_page.py
+++ pages/authentication/registration_page.py
@@ -3,6 +3,7 @@ from pages.base_page import BasePage
 from components.authentication.registration_form_component import RegistrationFormComponent
 from elements.button import Button
 from elements.link import Link
+import re
 
 class RegistrationPage(BasePage):
     def __init__(self, page: Page):
@@ -10,8 +11,13 @@ class RegistrationPage(BasePage):
 
         self.registration_form_component = RegistrationFormComponent(page)
 
+        self.login_link = Link(page, 'registration-page-login-link', 'Login')
         self.registration_button = Button(page,'registration-page-registration-button', 'Registration')
         self.login_link = Link(page,'registration-page-login-link', 'Login')
+
+    def click_login_link(self):
+        self.login_link.click()
+        self.check_current_url(re.compile('.*/#/auth/login'))
 
     def click_registration_button(self):
         self.registration_button.click()
